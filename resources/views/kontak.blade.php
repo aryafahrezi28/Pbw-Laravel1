bc4db54858f3a6b3e50418ee16bc6001d36cafdb
aryafahrezi28 <aryafahrezi28@gmail.com>
2025-10-03 16:34:50 +0700

    first commit

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang di LaraPress</title>
+</head>
+<body>
+    <h1>Selamat Datang di kontak LaraPress</h1>
+    <p>Email=aryafahrezi28@gmail.com</p>
+    <p>NoTelp=085715832137</p>
+    <a href="/">Kembali ke Halaman Utama</a>
+</body>
+</html>
